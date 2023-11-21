fbe7d59d9f44be69af3ac81756a1d3d4043537db
Maryannewambui <trimbleleewambui@gmail.com>
2023-11-21 21:18:56 +0300

    assignment complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -9,12 +9,13 @@ even = return_evens(numbers_0_to_20)
 
 print(even)
 
+
 def make_exclamation(sentence_list):
     exclamation_list = [sentence + "!" for sentence in sentence_list]
     return exclamation_list
 
 #example
-example_sentence = ["Hello. world"]
+example_sentence = ["Hello, world"]
 result = make_exclamation(example_sentence)
 
 print(result)
